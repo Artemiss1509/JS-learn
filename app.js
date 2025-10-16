1e28b00beb81bc83a6e8a4056b03240ebd265479
Artemiss1509 <stanza-grout54@icloud.com>
2025-10-16 16:22:19 +0530

    Clean-up

--- app.js
+++ app.js
@@ -1,45 +1,8 @@
 const http = require('http')
-const fs=require('fs')
+const routes = require('./routes')
 
-const server = http.createServer((req,res)=>{
-    const url = req.url
-    const method = req.method
 
-    if(req.url === '/'){
-        res.setHeader('Content-Type','text/html')
-        fs.readFile('message.txt',(err,data)=>{
-                    res.end(
-                        `
-                        <h1> Hello, ${data.toString()} </h1>
-                        <form action="/message" method="POST">
-                        <label>Name:</label>
-                        <input type="text" name="name">
-                        <button type="submit">Submit</button>
-                        </form>
-                        `
-                    )
-
-            })
-
-    }else{ 
-        if(req.url === '/message'){
-            res.setHeader('Content-Type','text/html');
-            const body = []
-            req.on('data',(chunk)=>{
-                body.push(chunk)
-            })
-            req.on('end',()=>{
-                const parsedBody = Buffer.concat(body).toString()
-                const message = parsedBody.split('=')[1]
-                fs.writeFile('message.txt',message,(err)=>{
-                    res.statusCode = 302
-                    res.setHeader('Location','/')
-                    return res.end()
-                })
-            })
-        }
-}
-})
+const server = http.createServer(routes)
 
 server.listen(3000,()=>{
     console.log('Server is running')
