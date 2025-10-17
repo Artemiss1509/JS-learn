e44307c522ae31cb2e57c9ba53366273ece14d59
Artemiss1509 <stanza-grout54@icloud.com>
2025-10-17 19:00:18 +0530

    Middle-ware-1

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 
 const app = express()
 
-app.use((res,req,next)=>{
-    console.log('Server is up and running on port 3000! Ready to handle requests.')
+app.use((req,res,next)=>{
+    req.user = 'Rohan'
+    next()
+})
+
+app.use('/welcome',(req,res,next)=>{
+    res.setHeader('Content-Type','text/html')
+    res.end(
+        `
+        <h1> Welcome ${req.user} </h1>
+        `
+    )
+    next()
 })
 
 app.listen(3000)
