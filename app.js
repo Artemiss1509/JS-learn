0364feff96dde62d2248aaa420c3d3d371fed7e6
Artemiss1509 <stanza-grout54@icloud.com>
2025-10-18 06:28:26 +0530

    Unknown-route

--- app.js
+++ app.js
@@ -4,71 +4,39 @@ const app = express()
 
 app.use(express.json())
 
-const users = [
-    {
-        'name':'Rohan'
-
-    },
-    {
-        'name':'Tyan'
-    },
-    {
-        'name':'Sara'
-    }
-]
-let newUsers = users.map(user => `<p> ${user.name} </p>`).join('')
 
 
-app.get('/users',(req,res)=>{
+app.get('/products',(req,res)=>{
     res.setHeader('Content-Type','text/html')
     
 
-    res.end(`<h1> Here is the list of all users. </h1>
-        
-        ${newUsers}`)
+    res.end(`<h1> Here is the list of all products. </h1>`)
 })
 
-app.post('/users',(req,res)=>{
-    const user = req.body
-    users.push(user)
-    newUsers = users.map(user => `<p> ${user.name} </p>`).join('')
-    res.json(user)
+app.post('/products',(req,res)=>{
+    const products = 'A new product has been added.'
+    res.json(products)
 })
 
 
-const orders = [
-    {
-        'name':'Order1'
-
-    },
-    {
-        'name':'Order2'
-    },
-    {
-        'name':'Order3'
-    }
-]
-
-let newOrders = orders.map(order => `<p> ${order.name} </p>`).join('')
-
-
-app.get('/orders',(req,res)=>{
+app.get('/categories',(req,res)=>{
     res.setHeader('Content-Type','text/html')
     
 
-    res.end(`<h1> Here is the list of all orders. </h1>
-        
-        ${newOrders}`)
+    res.end(`<h1> Here is the list of all categories. </h1>`)
 })
 
-app.post('/orders',(req,res)=>{
-    const order = req.body
-    orders.push(order)
-    newOrders = orders.map(order => `<p> ${order.name} </p>`).join('')
-    res.json(order)
+app.post('/categories',(req,res)=>{
+    const categories = 'A new category has been created.'
+    res.json(categories)
+})
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1> 404 - Page not found</h1>');
+
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is running on http://localhost:3000")
+app.listen(4000,()=>{
+    console.log("Server is running on http://localhost:4000")
 })
